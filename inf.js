e2f11c50fbfa737fa60474cf980bc18b599b2109
Mohammad <mzrahman74@gmail.com>
2019-12-30 19:15:54 -0600

    some update

---
+++ inf.js
@@ -0,0 +1,37 @@
+// Change heading:
+document.getElementById("myH").innerHTML = "Naushin && Nuhaa";
+
+document.getElementById("myP").innerHTML = "My interview today was very good. I did very well";
+
+function myFunction(){
+
+    document.getElementById("demo").innerHTML ="Hello JavaScript!";
+}
+
+let x,y, z;
+x=15;
+y=10;
+z=x+y;
+
+document.getElementById("demo2").innerHTML= "The value of z is " + z +" . ";
+
+// Function Declaration 
+
+function walk () {
+
+    console.log ('walk');
+
+    }
+
+// Anonymous Function Express 
+
+const run = function () {
+
+    console.log('run');
+};
+
+let move = run;
+run();
+move();
+
+
